32f884bbdd36e615ec40ac3c3a445a913b3ca827
Bahadur Khan <70307617+SwiftSoft-Bahadur@users.noreply.github.com>
2022-03-06 10:33:52 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,3 +13,57 @@ class Employee {
 
 let emp = new Employee(100, "Bahadur Khan Pathan")
 emp.show()
+
+
+// variable scoping rules
+// var let and const
+// Hoisting
+// shadowing
+// blocked-scope
+// local scope
+// script scope
+// global scope
+
+// named function
+// expression stype function
+// anonymous function
+// callback function
+// IIFE function
+// Higher order function
+// arrow function
+// function with return
+// function with without return
+// function with params
+// function with args
+// polifill for bind
+// closers
+// carrying function
+// debouncing function
+// throttle function
+
+// prototype
+// prototype chain
+// prototype property
+// prototype function
+// prototype class
+// polifill for map,filter and so on.
+// prototype-inheritance
+
+// user-defined array
+// user-defined object
+// user-defined class
+
+// class
+// constructor
+// object creation
+// inheritance
+//
+
+// call(), apply(),bind()
+// promise
+// Synchronous
+// Asynchronous
+// closers
+// carrying function
+// no carrying
+
